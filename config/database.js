9fabd78240b47bff447c4d70f8272b6a954362c8
hhj <sbs98010@naver.com>
2022-12-01 01:01:45 +0900

    엔드포인트

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '192.128.219.103',
-    user: 'root',
+    host: 'database-1.cj65iposkrib.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
     port: '3306',
-    password: 'nrb123',
+    password: 'nrb12345',
     database: 'nrb'
 });
 
