9911124044ced9d0e32e674b5c313bf50f4ade04
hhj <sbs98010@naver.com>
2022-12-03 03:53:21 +0900

    auth빼고 다건듬

--- src/app/User/userDao.js
+++ src/app/User/userDao.js
@@ -1,10 +1,16 @@
-// userPost 조회
-async function selectUserPost(connection, uIdx,dayIdx) {
-    const selectUserIdxQuery = `
-                   SELECT uIdx,dayIdx,level1A,level1NumE,level2A,level2NumE,level3A,level3NumE
-                   FROM Record 
-                   WHERE uIdx = ? and dayIdx = ?;
-                   `;
-    const [userRow] = await connection.query(selectUserIdxQuery, uIdx, dayIdx);
-    return userRow;
-  }
+// 유저 생성
+async function insertUserInfo(connection, insertUserInfoParams) {
+  const insertUserInfoQuery = `
+        INSERT INTO UserInfo(uName)
+        VALUES (?);
+    `;
+  const insertUserInfoRow = await connection.query(
+    insertUserInfoQuery,
+    insertUserInfoParams
+  );
+
+  return insertUserInfoRow;
+}
+
+  module.exports = {
+    insertUserInfo  };
